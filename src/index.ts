7481ee9f425b26e765ae8033b28c99799dd7d3f4
Ophir Bucai <ophirbucai@gmail.com>
2025-05-06 01:58:56 +0300

    chore: Fix formatting

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { PreviewOptions } from "./types";
 /**
  * ##### Previews a file on iOS using Quicklook.
  *
- * **Platform Compatibility: ** This module is [_currently_](https://github.com/PierreJanineh-com/expo-quicklook-preview/issues/2) only works on iOS.
+ * **Platform Compatibility:** This module is [_currently_](https://github.com/PierreJanineh-com/expo-quicklook-preview/issues/2) only works on iOS.
  * On Android devices, calling this function will do nothing.
  */
 export async function preview(options: PreviewOptions) {
