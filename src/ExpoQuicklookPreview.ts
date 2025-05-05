7e5793a5a54f55b3c80348f03a287f93fb4af91e
Ophir Bucai <ophirbucai@gmail.com>
2025-05-05 22:36:10 +0300

    chore: Remove unneeded nl

--- src/ExpoQuicklookPreview.ts
+++ src/ExpoQuicklookPreview.ts
@@ -2,7 +2,6 @@ import { NativeModule, requireOptionalNativeModule } from "expo";
 
 import type { PreviewOptions } from "./types";
 
-
 declare class ExpoQuicklookPreviewModule extends NativeModule {
   preview(options: PreviewOptions): Promise<void>;
 }
